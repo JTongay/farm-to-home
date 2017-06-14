debd50268974208360831027a6db9580b507e1d2
FredCamaragit <fcamara14@gmail.com>
2017-06-13 20:48:44 -0500

    Added models for Users and Farms

---
+++ models/farms.js
@@ -0,0 +1,42 @@
+module.exports = function(sequelize, DataTypes){
+  var Farm = sequelize.define("Event", {
+    name: {
+      type: DataTypes.STRING,
+
+    }
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+    // items: [
+    //   {
+    //     name: "milk",
+    //     price: 1,
+    //     quantity: 100
+    //   }
+    // ]
+  },
+  {
+    // We're saying that we want our Author to have Posts
+    classMethods: {
+      associate: function(models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        Event.belongsTo(models.User, {
+          as: "Creator",
+          foreignKey: {
+            name: "creatorId",
+            allowNull: false,
+          }
+        });
+      }
+    }
+};
---
+++ models/users.js
@@ -0,0 +1,50 @@
+var Sequelize = require('sequelize');
+
+module.exports = {
+  var User = sequelize.define("User", {
+   oauthId: {
+     type: DataTypes.STRING,
+    // allowNull: true
+   },
+   name: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   email: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   address: {
+     type: DataTypes.String,
+     allowNull: true
+   },
+   dateAdded: {
+     type: DataTypes.DATE,
+     allowNull: false,
+     defaultValue: Sequelize.NOW
+   },
+   credType: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   password: {
+     type: DataTypes.STRING,
+     allowNull: true
+   },
+   classMethods: {
+     associate: function(models) {
+       // Associating Author with Posts
+       // When an Author is deleted, also delete any associated Posts
+       User.hasMany(models.Event,
+         {
+          foreignKey: {
+           name: "creatorId",
+           allowNull: false,
+         }
+       });
+    }
+    return User;
+}
+
+
+module.exports = User;
